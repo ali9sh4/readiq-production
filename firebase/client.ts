09eaa004bb6e0b266edf3911308763a4307772ff
ali9sh4 <ali9sh4@gmail.com>
2025-11-17 02:16:34 +0300

    ilove my self

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,20 +4,17 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCmjn2Enchkf-BH3-dBuBfCJPKPDnqfeT8",
-    authDomain: "readiq-1f109.firebaseapp.com",
-    projectId: "readiq-1f109",
-    storageBucket: "readiq-1f109.firebasestorage.app",
-    messagingSenderId: "338348007207",
-    appId: "1:338348007207:web:3a02400113057ea6455302",
-    measurementId: "G-5GX4VE53WX"
-  };
-// Initialize Firebase only once  
-const app = !getApps().length? initializeApp(firebaseConfig):getApp()
-    //export initilaized services
-    export const auth = getAuth(app);
-    export const storage = getStorage(app);
-     export const db = getFirestore(app);
-    
-
-
+  apiKey: "AIzaSyCmjn2Enchkf-BH3-dBuBfCJPKPDnqfeT8",
+  authDomain: "readiq-1f109.firebaseapp.com",
+  projectId: "readiq-1f109",
+  storageBucket: "readiq-1f109.firebasestorage.app",
+  messagingSenderId: "338348007207",
+  appId: "1:338348007207:web:3a02400113057ea6455302",
+  measurementId: "G-5GX4VE53WX",
+};
+// Initialize Firebase only once
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+//export initilaized services
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
