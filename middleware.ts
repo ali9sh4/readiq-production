9f32248f4b44515915758d1393b2e2b473f6f188
Ali Suhail <87365635+ali9sh4@users.noreply.github.com>
2025-11-17 17:42:09 +0300

    Update middleware.ts with deleted matcher login and course upload

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,66 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { jwtVerify, createRemoteJWKSet } from "jose";
+
+const JWKS = createRemoteJWKSet(
+  new URL(
+    "https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com"
+  )
+);
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const cookieStore = await cookies();
-  const token = cookieStore.get("firebaseAuthToken")?.value;
 
-  // If on /login and has token → go to home
-  if (pathname.startsWith("/login") && token) {
+  if (pathname.startsWith("/login")) {
+    const cookie = await cookies();
+    const token = cookie.get("firebaseAuthToken")?.value;
+
+    if (token) {
+      try {
+        await jwtVerify(token, JWKS, {
+          issuer: `https://securetoken.google.com/readiq-1f109`,
+          audience: "readiq-1f109",
+        });
+        return NextResponse.redirect(new URL("/", request.url));
+      } catch (error) {
+        console.log(error);
+        return NextResponse.next();
+      }
+    }
+    return NextResponse.next();
+  }
+
+  try {
+    const cookie = await cookies();
+    const token = cookie.get("firebaseAuthToken")?.value;
+
+    if (!token) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const { payload } = await jwtVerify(token, JWKS, {
+      issuer: `https://securetoken.google.com/readiq-1f109`,
+      audience: "readiq-1f109",
+    });
+
+    if (pathname.startsWith("/admin-dashboard")) {
+      if (!payload.admin) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+    }
+
+    const response = NextResponse.next();
+    response.headers.set("x-user-id", payload.sub || "");
+    response.headers.set("x-user-email", String(payload.email || ""));
+    response.headers.set("x-user-admin", String(payload.admin === true));
+
+    return response;
+  } catch (error) {
+    console.log(error);
     return NextResponse.redirect(new URL("/", request.url));
   }
-
-  // If on protected route and NO token → go to sign-in
-  if (!pathname.startsWith("/login") && !token) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
-
-  // Otherwise, let them through
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin-dashboard/:path*", "/login", "/course-upload/:path*"],
+  matcher: ["/admin-dashboard/:path*"],
 };
